7a07cea4f3522b2015bf5cae9f902b30b3b4e644
#khansofficial <khanmahdi786@gmail.com>
2020-02-29 09:14:08 +0530

    final Build

--- dist/main.js
+++ dist/main.js
@@ -1,8 +1,8 @@
 //cashing the Dom
-const arabicVerse = document.querySelector('.arabic-verse');
-const urduVerse = document.querySelector('.urdu-verse');
-const engVerse = document.querySelector('.eng-verse');
-const ayahNo = document.querySelector('.ayah-no');
+const arabicVerse = document.querySelector(".arabic-verse");
+const urduVerse = document.querySelector(".urdu-verse");
+const engVerse = document.querySelector(".eng-verse");
+const ayahNo = document.querySelector(".ayah-no");
 const introPage = document.querySelector(".intro-page");
 const entryButton = document.querySelector(".entry-button");
 const menuToggle = document.querySelector(".menubar");
@@ -20,33 +20,34 @@ const sliderImages = document.querySelectorAll(".slide-img");
 let slideImagesCount = sliderImages.length - 1;
 let sliderCount = 0;
 let currentImgWidth = sliderImages[sliderCount].offsetWidth;
-let quranVersesArabic = ['وَقَالَ رَبُّكُمُ ادْعُونِي أَسْتَجِبْ لَكُمْ ۚ إِنَّ الَّذِينَ يَسْتَكْبِرُونَ عَنْ عِبَادَتِي سَيَدْخُلُونَ جَهَنَّمَ دَاخِرِينَ',
-                          'وَمَا كَانَ اللَّهُ لِيُعَذِّبَهُمْ وَأَنتَ فِيهِمْ ۚ وَمَا كَانَ اللَّهُ مُعَذِّبَهُمْ وَهُمْ يَسْتَغْفِرُونَ - ',
-                        'فَاذْكُرُونِي أَذْكُرْكُمْ وَاشْكُرُوا لِي وَلَا تَكْفُرُونِ - 2:152',
-                      'يَعْلَمُ مَا فِي السَّمَاوَاتِ وَالْأَرْضِ وَيَعْلَمُ مَا تُسِرُّونَ وَمَا تُعْلِنُونَ ۚ وَاللَّهُ عَلِيمٌ بِذَاتِ الصُّدُورِ - 64:4',
-                      'وَيَرْزُقْهُ مِنْ حَيْثُ لَا يَحْتَسِبُ ۚ وَمَن يَتَوَكَّلْ عَلَى اللَّهِ فَهُوَ حَسْبُهُ ۚ إِنَّ اللَّهَ بَالِغُ أَمْرِهِ ۚ قَدْ جَعَلَ اللَّهُ لِكُلِّ شَيْءٍ قَدْرًا - 65:3',
-                    ''];
-let quranicVersesEnglish = ['And your Lord says, "Call upon Me; I will respond to you." Indeed, those who disdain My worship will enter Hell [rendered] contemptible.',
-"But Allah would not punish them while you, [O Muhammad], are among them, and Allah would not punish them while they seek forgiveness.",
-'So remember Me; I will remember you. And be grateful to Me and do not deny Me',
-'He knows what is within the heavens and earth and knows what you conceal and what you declare. And Allah is Knowing of that within the chest.',
-'And will provide for him from where he does not expect. And whoever relies upon Allah - then He is sufficient for him. Indeed, Allah will accomplish His purpose. Allah has already set for everything a [decreed] extent.',
-''];
-let quranicVersesUrdu = ['اور آپ کا پروردگار کہتا ہے کہ مجھے پکار لو میں تمہیں جواب دوں گا۔ بے شک جو لوگ میری عبادت سے نفرت کرتے ہیں وہ دوزخ میں داخل ہوں گے',
-'لیکن اللہ ان کو عذاب نہیں دیتا جب کہ تم بھی ان میں شامل ہو اور اللہ معافی مانگنے پر انہیں عذاب نہیں دیتا۔',
-'تو مجھے یاد کرو۔ میں تمہیں یاد رکھوں گا. اور مجھ پر احسان کرو اور مجھ سے انکار نہ کرو',
-'وہی جانتا ہے جو کچھ آسمانوں اور زمین میں ہے اور وہ جانتا ہے جو تم پوشیدہ کرتے ہو اور جو تم اعلان کرتے ہو۔ اور اللہ اس کو سینے میں جانتا ہے۔',
-'اور اس کے لئے رزق فراہم کرے گا جہاں سے وہ توقع نہیں کرتا ہے۔ اور جو شخص اللہ پر بھروسہ کرتا ہے تو وہ اس کے لئے کافی ہے۔ بے شک اللہ اپنے مقصد کو پورا کرے گا۔ اللہ نے ہر چیز کے لئے پہلے ہی ایک حد مقرر کردی ہے۔',
+let quranVersesArabic = [
+" قُلْ هَلْ يَسْتَوِي الَّذِينَ يَعْلَمُونَ وَالَّذِينَ لَا يَعْلَمُونَ ۗ إِنَّمَا يَتَذَكَّرُ أُولُو الْأَلْبَابِ - 39:9",
+  "وَمَا كَانَ اللَّهُ لِيُعَذِّبَهُمْ وَأَنتَ فِيهِمْ ۚ وَمَا كَانَ اللَّهُ مُعَذِّبَهُمْ وَهُمْ يَسْتَغْفِرُونَ - ",
+  "فَاذْكُرُونِي أَذْكُرْكُمْ وَاشْكُرُوا لِي وَلَا تَكْفُرُونِ - 2:152",
+  "يَعْلَمُ مَا فِي السَّمَاوَاتِ وَالْأَرْضِ وَيَعْلَمُ مَا تُسِرُّونَ وَمَا تُعْلِنُونَ ۚ وَاللَّهُ عَلِيمٌ بِذَاتِ الصُّدُورِ - 64:4",
+  "وَيَرْزُقْهُ مِنْ حَيْثُ لَا يَحْتَسِبُ ۚ وَمَن يَتَوَكَّلْ عَلَى اللَّهِ فَهُوَ حَسْبُهُ ۚ إِنَّ اللَّهَ بَالِغُ أَمْرِهِ ۚ قَدْ جَعَلَ اللَّهُ لِكُلِّ شَيْءٍ قَدْرًا - 65:3",
+  ""
 ];
-let quranicAyahNo = ['40:60',
-'8:33',
-'2:152',
-'64:4',
-'65:3']
+let quranicVersesEnglish = [
+  'Say, "Are those who know equal to those who do not know?" Only they will remember [who are] people of understanding.',
+  "But Allah would not punish them while you, [O Muhammad], are among them, and Allah would not punish them while they seek forgiveness.",
+  "So remember Me; I will remember you. And be grateful to Me and do not deny Me",
+  "He knows what is within the heavens and earth and knows what you conceal and what you declare. And Allah is Knowing of that within the chest.",
+  "And will provide for him from where he does not expect. And whoever relies upon Allah - then He is sufficient for him. Indeed, Allah will accomplish His purpose. Allah has already set for everything a [decreed] extent.",
+  ""
+];
+let quranicVersesUrdu = [
+"Say, Are those who know equal to those who do not know? Only they will remember [who are] people of understanding.",
+  "لیکن اللہ ان کو عذاب نہیں دیتا جب کہ تم بھی ان میں شامل ہو اور اللہ معافی مانگنے پر انہیں عذاب نہیں دیتا۔",
+  "تو مجھے یاد کرو۔ میں تمہیں یاد رکھوں گا. اور مجھ پر احسان کرو اور مجھ سے انکار نہ کرو",
+  "وہی جانتا ہے جو کچھ آسمانوں اور زمین میں ہے اور وہ جانتا ہے جو تم پوشیدہ کرتے ہو اور جو تم اعلان کرتے ہو۔ اور اللہ اس کو سینے میں جانتا ہے۔",
+  "اور اس کے لئے رزق فراہم کرے گا جہاں سے وہ توقع نہیں کرتا ہے۔ اور جو شخص اللہ پر بھروسہ کرتا ہے تو وہ اس کے لئے کافی ہے۔ بے شک اللہ اپنے مقصد کو پورا کرے گا۔ اللہ نے ہر چیز کے لئے پہلے ہی ایک حد مقرر کردی ہے۔"
+];
+let quranicAyahNo = ["Al-Quran - 39:9", "8:33", "2:152", "64:4", "65:3"];
 //functions
 //quran verse selector
 const quranVerseGenerator = () => {
- let randomNumber = Math.floor(Math.random() * 4);
+  let randomNumber = Math.floor(Math.random() * 4);
   arabicVerse.innerHTML = quranVersesArabic[randomNumber];
   urduVerse.innerHTML = quranicVersesUrdu[randomNumber];
   engVerse.innerHTML = quranicVersesEnglish[randomNumber];
@@ -55,10 +56,10 @@ const quranVerseGenerator = () => {
 quranVerseGenerator();
 //intro page fading
 const introPageFade = () => {
-  introPage.classList.add('opacity-toggle');
+  introPage.classList.add("opacity-toggle");
   setTimeout(() => {
-    introPage.classList.add('display-toggle');
-  }, 500)
+    introPage.classList.add("display-toggle");
+  }, 1000);
 };
 
 // Appear function
@@ -66,16 +67,16 @@ const appear = () => {
   let aboutToTop = aboutPara.getBoundingClientRect().top;
   let contactToTop = contactPara.getBoundingClientRect().top;
   let screenHeight = window.innerHeight;
- 
-  if(aboutToTop < (screenHeight / 1.3)){
-    aboutPara.style.opacity = '1' ;
-    aboutPara.style.transform = 'translateY(0px)' ;
-  };
-  if(contactToTop < (screenHeight / 1.3)){
-   contactPara.style.opacity = '1';
-   contactPara.style.transform = 'translateY(0px)';
+
+  if (aboutToTop < screenHeight / 1.3) {
+    aboutPara.style.opacity = "1";
+    aboutPara.style.transform = "translateY(0px)";
   }
- };
+  if (contactToTop < screenHeight / 1.3) {
+    contactPara.style.opacity = "1";
+    contactPara.style.transform = "translateY(0px)";
+  }
+};
 
 //= menu toggler
 const menuToggler = () => {
@@ -125,5 +126,8 @@ leftBtn.addEventListener("click", rightSlide);
 rightBtn.addEventListener("click", leftSlide);
 leftBtn.addEventListener("click", stopSlider);
 rightBtn.addEventListener("click", stopSlider);
-entryButton.addEventListener('click', introPageFade);
-window.addEventListener('scroll', appear);
+entryButton.addEventListener("click", introPageFade);
+window.addEventListener("scroll", appear);
+
+
+const text = 'کیا وہ لوگ جو جانتے ہیں برابر ہیں ان کے جو نہیں جانتے ؟ صرف وہی لوگ (جو) سمجھدار لوگ ہیں یاد رکھیں گے۔'
